d7cbebf81bf104f951a3b688fa5d3f2066d456b4
dfSabogal10 <david.sabogal1@gmail.com>
2018-03-31 15:37:09 -0500

    Verde: test login

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -17,44 +17,6 @@ class FunctionalTest(TestCase):
         self.browser.get('http://localhost:8000')
         self.assertIn('Busco Ayuda', self.browser.title)
 
-    # def test_registro(self):
-    #     self.browser.get('http://localhost:8000')
-    #     link = self.browser.find_element_by_id('id_register')
-    #     link.click()
-    #
-    #     nombre = self.browser.find_element_by_id('id_nombre')
-    #     nombre.send_keys('David Felipe')
-    #
-    #     apellidos = self.browser.find_element_by_id('id_apellidos')
-    #     apellidos.send_keys('Sabogal')
-    #
-    #     experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
-    #     experiencia.send_keys('1')
-    #
-    #     self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
-    #     telefono = self.browser.find_element_by_id('id_telefono')
-    #     telefono.send_keys('3001234567')
-    #
-    #     correo = self.browser.find_element_by_id('id_correo')
-    #     correo.send_keys('df.sabogal10@uniandes.edu.co')
-    #
-    #     imagen = self.browser.find_element_by_id('id_imagen')
-    #     print()
-    #     imagen.send_keys(os.path.dirname(os.path.realpath(__file__))+'/static/atila.jpeg')
-    #
-    #     nombreUsuario = self.browser.find_element_by_id('id_username')
-    #     nombreUsuario.send_keys('felipe1')
-    #
-    #     clave = self.browser.find_element_by_id('id_password')
-    #     clave.send_keys('clave123')
-    #
-    #     botonGrabar = self.browser.find_element_by_id('id_grabar')
-    #     botonGrabar.click()
-    #     self.browser.implicitly_wait(3)
-    #     span=self.browser.find_element(By.XPATH, '//span[text()="David Felipe Sabogal"]')
-    #
-    #     self.assertIn('David Felipe Sabogal', span.text)
-
     def test_verDetalle(self):
         self.browser.get('http://localhost:8000')
         span=self.browser.find_element(By.XPATH, '//span[text()="David Felipe Sabogal"]')
@@ -69,11 +31,13 @@ class FunctionalTest(TestCase):
         link = self.browser.find_element_by_id('id_login')
         link.click()
 
-        nombreUsuario = self.browser.find_element_by_id('id_username')
-        nombreUsuario.send_keys('felipe1')
+        loginmodal=self.browser.find_element_by_id('login_modal')
+        nombreUsuario = loginmodal.find_element_by_id('id_username')
+        nombreUsuario.send_keys('felipe')
 
-        clave = self.browser.find_element_by_id('id_password')
-        clave.send_keys('clave123')
+
+        clave = loginmodal.find_element_by_id('id_password')
+        clave.send_keys('administrador')
 
         botonGrabar = self.browser.find_element_by_id('id_ingresar')
         botonGrabar.click()
@@ -81,4 +45,42 @@ class FunctionalTest(TestCase):
         self.browser.implicitly_wait(3)
         linkeditar = self.browser.find_element_by_id('id_editar')
         self.assertIn('Editar', linkeditar.text)
+
+    def test_registro(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_register')
+        link.click()
+
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('David Felipe')
+
+        apellidos = self.browser.find_element_by_id('id_apellidos')
+        apellidos.send_keys('Sabogal')
+
+        experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        experiencia.send_keys('1')
+
+        self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys('3001234567')
+
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('df.sabogal10@uniandes.edu.co')
+
+        imagen = self.browser.find_element_by_id('id_imagen')
+        print()
+        imagen.send_keys(os.path.dirname(os.path.realpath(__file__))+'/static/atila.jpeg')
+        registermodal=self.browser.find_element_by_id('register_modal')
+        nombreUsuario = registermodal.find_element_by_id('id_username')
+        nombreUsuario.send_keys('felipe1')
+
+        clave = registermodal.find_element_by_id('id_password')
+        clave.send_keys('clave123')
+
+        botonGrabar = self.browser.find_element_by_id('id_grabar')
+        botonGrabar.click()
+        self.browser.implicitly_wait(3)
+        span=self.browser.find_element(By.XPATH, '//span[text()="David Felipe Sabogal"]')
+
+        self.assertIn('David Felipe Sabogal', span.text)
 
