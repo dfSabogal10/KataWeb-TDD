79bd73052abb0e610705e2c585ada556a032b6c8
dfSabogal10 <david.sabogal1@gmail.com>
2018-03-31 13:24:19 -0500

    Verde: test titulo de la pagina pasa

--- buscoayuda4101/settings.py
+++ buscoayuda4101/settings.py
@@ -108,8 +108,8 @@ WSGI_APPLICATION = 'buscoayuda4101.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
-        'NAME': os.environ.get('DB_NAME'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
